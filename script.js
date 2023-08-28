44861fecaafb57eb23cb2b03d9d7dc246b458e6a
128bit <128bit@eclipso.eu>
2023-08-28 18:15:47 +0300

    Add function to populate the bookshelf

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const books = [];
+populateShelf();
 
 function Book(name, author, description, pageCount) {
     this.name = name;
@@ -11,13 +12,7 @@ function Book(name, author, description, pageCount) {
 
 const dialog = document.querySelector("dialog");
 const closeBtn = document.getElementById("cancel");
-const openBtn = document.getElementById("new-book");
 const form = document.querySelector("dialog > form");
-
-openBtn.addEventListener("click", () => {
-    dialog.showModal();
-    form.reset();
-});
 
 closeBtn.addEventListener("click", () => dialog.close());
 
@@ -28,4 +23,46 @@ form.addEventListener("submit", () => {
     const pageCount = document.getElementById("page-count").value;
     const book = new Book(name, author, description, pageCount);
     books.push(book);
+    populateShelf();
 });
+
+function populateShelf() {
+    const shelf = document.querySelector(".books");
+    shelf.innerHTML = "";
+    books.forEach((book, index) => {
+        const bookCardTemplate = `
+       <div class="image-wrapper" data-index="${index}">
+         <svg class="image image-done hidden">
+           <use class="image image-done" href="./done.svg#done"></use>
+         </svg>
+         <svg class="image book-icon">
+           <use class="image" href="./book-icon.svg#book-icon"></use>
+         </svg>
+       </div>
+       <p class="title fw-bold">
+            ${book.name}
+       </p>
+       <p class="author">${book.author}</p>
+       <p class="pages">${book.pageRead}/${book.pageCount}</p>
+       <p class="progress">${(book.pageRead / book.pageCount) * 100}%</p>`;
+
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("card", "bg-secondary");
+        bookCard.innerHTML = bookCardTemplate;
+        shelf.appendChild(bookCard);
+    });
+    const addBookCardTemplate = `
+        <svg class="image add-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <title>plus</title>
+            <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+        </svg>`;
+    const addBookCard = document.createElement("div");
+    addBookCard.setAttribute("id", "new-book");
+    addBookCard.classList.add("add-book");
+    addBookCard.innerHTML = addBookCardTemplate;
+    shelf.appendChild(addBookCard);
+    addBookCard.addEventListener("click", () => {
+        dialog.showModal();
+        form.reset();
+    });
+}
