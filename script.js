391a1159e39d12f7391600a6aaa834145aae5a84
128bit <128bit@eclipso.eu>
2023-08-28 16:34:26 +0300

    Add dialog form processing

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const books = [];
+
 function Book(name, author, description, pageCount) {
     this.name = name;
     this.author = author;
@@ -7,7 +9,23 @@ function Book(name, author, description, pageCount) {
     this.description = description;
 }
 
-const close = document.getElementById("cancel");
-const open = document.getElementById("new-book");
 const dialog = document.querySelector("dialog");
-open.addEventListener("click", () => dialog.showModal());
+const closeBtn = document.getElementById("cancel");
+const openBtn = document.getElementById("new-book");
+const form = document.querySelector("dialog > form");
+
+openBtn.addEventListener("click", () => {
+    dialog.showModal();
+    form.reset();
+});
+
+closeBtn.addEventListener("click", () => dialog.close());
+
+form.addEventListener("submit", () => {
+    const name = document.getElementById("book-name").value;
+    const author = document.getElementById("author").value;
+    const description = document.getElementById("description").value;
+    const pageCount = document.getElementById("page-count").value;
+    const book = new Book(name, author, description, pageCount);
+    books.push(book);
+});
